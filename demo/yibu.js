54450027a7efafeeb0cac3dd9aaa9d40f57f1b9e
Angeliz <471330418@qq.com>
2017-11-08 10:46:31 +0800

    脚本同步异步延迟测试

---
+++ demo/yibu.js
@@ -0,0 +1,6 @@
+ window.onload=function(){	
+ 	document.write("<p>这是js里面的内容</p>");
+ 	document.write("<p>这是第二个js的内容</p>");
+ };
+// document.write("<p>这是js里面的内容</p>");
+// document.write("<p>这是第二个js的内容</p>");
